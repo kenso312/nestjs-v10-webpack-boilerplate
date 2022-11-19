f5f1ac725c036e23506f2f3caced64120c5d29c7
Ken So <sotszyau312@gmail.com>
2022-11-19 18:34:59 +0800

    chore(deps): upgrade dependency packages

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -27,6 +27,6 @@ export const initialize = (app: INestApplication) => {
   // For Swagger UI
   if (NODE_ENV === NodeEnv.DEVELOPMENT) app.enableCors();
 
-  // For convenience
+  // For convenience exclude to set base path when doing e2e test
   if (BASE_PATH && NODE_ENV !== NodeEnv.TEST) app.setGlobalPrefix(BASE_PATH);
 };
