e4ec4c5ae3f9e0566e2ddaccbd0729ece7bf3157
Ken So <sotszyau312@gmail.com>
2023-02-12 23:24:07 +0800

    feat: added repl mode

---
+++ src/utils/repl.ts
@@ -0,0 +1,17 @@
+import { AppModule } from '@/modules/app';
+import { Logger } from '@nestjs/common';
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
+import { repl } from '@nestjs/core';
+
+(async () => {
+  const replServer = await repl(AppModule);
+  const logger = new Logger();
+
+  const cacheDir = join('node_modules', '.cache');
+  if (!existsSync(cacheDir)) mkdirSync(cacheDir);
+
+  replServer.setupHistory(join(cacheDir, '.nestjs_repl_history'), (err) => {
+    if (err) logger.error(err);
+  });
+})();
